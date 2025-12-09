594ccfc4ea612e7840d965eda6380f55b548bd19
kbmhansungb <kbmhansungb@gmail.com>
2025-12-09 15:48:32 +0900

    uuid 수정

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid/v4');
 const fs = require('fs');
 const path = require('path');
 
